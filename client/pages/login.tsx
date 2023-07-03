c92cf9bd5009d3849b0d5872c5d8f9614322e40c
Florent Paris <florentparis28@gmail.com>
2023-07-03 19:02:32 +0200

    🌈 style: auth pages mobile

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -4,6 +4,7 @@ import fonts from '../styles/fonts.module.scss';
 import FormField from '@/components/FormField';
 import Button from '@/components/Button';
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface FormData {
   email: string;
@@ -36,6 +37,24 @@ export default function Login() {
             <div className={style.first_line}></div>
             <div className={style.circle}></div>
             <div className={style.second_line}></div>
+          </div>
+          <div className={style.sign_other_service_desktop}>
+            <span className={fonts.paragraph_medium}>Or sign in with</span>
+            <div>
+              <button>
+                <Image src="/icons/fb.svg" alt="me" width="25" height="25" />
+                <span>Facebook</span>
+              </button>
+              <button>
+                <Image
+                  src="/icons/google.png"
+                  alt="me"
+                  width="25"
+                  height="25"
+                />
+                <span>Google</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -70,6 +89,25 @@ export default function Login() {
             </Link>
           </p>
         </form>
+        <div className={style.sign_other_service_mobile}>
+          <span className={fonts.paragraph_medium}>Or sign in with</span>
+          <div>
+            <button>
+              <Image src="/icons/fb.svg" alt="me" width="25" height="25" />
+              <span>Facebook</span>
+            </button>
+            <button>
+              <Image src="/icons/google.png" alt="me" width="25" height="25" />
+              <span>Google</span>
+            </button>
+          </div>
+          <p className={fonts.paragraph_medium}>
+            You don't have an account?{' '}
+            <Link href="/register" className={fonts.paragraph_semi_bold}>
+              Register
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,9 +1,10 @@
+import { FormEvent, useState } from 'react';
 import Button from '@/components/Button';
 import FormField from '@/components/FormField';
 import style from '../styles/pages/auth.module.scss';
 import fonts from '../styles/fonts.module.scss';
-import { ChangeEvent, FormEvent, useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -26,10 +27,34 @@ export default function Register() {
     <div className={style.container}>
       <div>
         <div className={style.background}>
+          <div className={style.first_line}></div>
+          <div className={style.second_line}></div>
+          <div className={style.head_mobile}>
+            <h1 className={fonts.head}>Sign up</h1>
+            <p className={fonts.paragraph_regular}>Create your account</p>
+          </div>
           <div className={style.circle}>
             <div className={style.first_line}></div>
             <div className={style.circle}></div>
             <div className={style.second_line}></div>
+          </div>
+          <div className={style.sign_other_service_desktop}>
+            <span className={fonts.paragraph_medium}>Or sign in with</span>
+            <div>
+              <button>
+                <Image src="/icons/fb.svg" alt="me" width="25" height="25" />
+                <span>Facebook</span>
+              </button>
+              <button>
+                <Image
+                  src="/icons/google.png"
+                  alt="me"
+                  width="25"
+                  height="25"
+                />
+                <span>Google</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -77,6 +102,25 @@ export default function Register() {
             </Link>
           </p>
         </form>
+        <div className={style.sign_other_service_mobile}>
+          <span className={fonts.paragraph_medium}>Or sign in with</span>
+          <div>
+            <button>
+              <Image src="/icons/fb.svg" alt="me" width="25" height="25" />
+              <span>Facebook</span>
+            </button>
+            <button>
+              <Image src="/icons/google.png" alt="me" width="25" height="25" />
+              <span>Google</span>
+            </button>
+          </div>
+          <p className={fonts.paragraph_medium}>
+            You already have an account?{' '}
+            <Link href="/login" className={fonts.paragraph_semi_bold}>
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
