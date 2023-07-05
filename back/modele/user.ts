5a18d672f637b79c500e8dda8ca6cc724429a9c5
Ilyes <ilyesb3613@gmail.com>
2023-07-05 12:05:29 +0200

    ✨ feat: users gestion

---
+++ back/modele/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    user_id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    phone: string;
+    notification: string;
+    instruments: string;
+    status: string;
+  }
+  
