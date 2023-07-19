90752fc27948e7706335994761c69631fc4ebec8
ilyesb36 <ilyesb36130@gmail.com>
2023-07-19 11:44:37 +0200

    fix gitignore + back locally

--- back/db/database.ts
+++ back/db/database.ts
@@ -1,7 +1,16 @@
 import { Pool, Client } from 'pg';
 
+// Connexion via docker
+// const pool = new Pool({
+//   host: 'db',
+//   user: 'your_username',
+//   password: 'your_password',
+//   database: 'your_database'
+// });
+
 const pool = new Pool({
-  host: 'db',
+  host: 'localhost',
+  port: 5432,
   user: 'your_username',
   password: 'your_password',
   database: 'your_database'
