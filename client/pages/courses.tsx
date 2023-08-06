0e88d301e1635a4db86e496971ab5194eb09963d
Giselle MM <giselle.magalong@gmail.com>
2023-08-06 15:50:42 +0200

    Update filter style

--- client/pages/courses.tsx
+++ client/pages/courses.tsx
@@ -2,21 +2,20 @@ import Header from '@/components/common/Header';
 import style from '../styles/pages/listing.module.scss';
 import CardContainerStyle from '../styles/components/CardContainer.module.scss';
 import Button from '@/components/Button';
-import Image from 'next/image';
 import Card from '@/components/Card';
 import CardContainer from '@/components/CardContainer';
 import CourseCard from '@/components/CourseCard';
-import ActivityCard from '@/components/ActivityCard';
-import CompetitionCard from '@/components/CompetitionCard';
-import ArticleCard from '@/components/ArticleCard';
 import { useEffect, useState } from 'react';
 import useWindowSize from '@/hooks/useWindowSize';
-import InstrumentIcon from "@/components/InstrumentIcon";
 import FormField from "@/components/FormField";
+import Modal from "@/components/Modal";
+import {Simulate} from "react-dom/test-utils";
+import toggle = Simulate.toggle;
 
 export default function Dashboard() {
     const size = useWindowSize();
     const [widthContainerArticles, setWidthContainerArticles] = useState(0);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         handleResize();
@@ -32,38 +31,117 @@ export default function Dashboard() {
         }
     };
 
+    const handleOpen = (toggle) => {
+        setOpen(toggle)
+    }
+
+    console.log(open);
     return (
         <div className={style.listing}>
             <Header name="Courses" />
             <CardContainer>
                 <div className={CardContainerStyle.rowReverse}>
                     <div className={style.search}>
-                        <div> input recherche</div>
-                        <div className={style.filterButton}>
-                            <Button size={'sm'}>Filter</Button>
+                        <FormField
+                            label="Search"
+                            type="search"
+                            size="md"
+                            value=""
+                        />
+                        <div className={style.filterButton} onClick={() => handleOpen(true)} style={open?{zIndex: 0}:{zIndex:1}}>
+                            <Button size={'sm'} >Filter</Button>
                         </div>
-
+                        <Modal setOpen={handleOpen} open={open}>
+                            <div className={style.mobile_filter_content}>
+                                <FormField
+                                    label="Professor"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="Composer"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="Composer"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="State"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="Display"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                            </div>
+                            <div className={style.mobile_submit_filter}>
+                                <a href="">Undo Filter</a>
+                                <Button size={'sm'}>Filter</Button>
+                            </div>
+                        </Modal>
                     </div>
-                    <div> instrument filtre</div>
+                    <FormField
+                        label="Search instrument"
+                        type="search"
+                        size="md"
+                        value=""
+                    />
                 </div>
                 <div className={CardContainerStyle.rowReverse}>
                     <div className={style.desktop_filter}>
-                        <div className={CardContainerStyle.column}>
-                            <Card
-                                title="Filter"
-                                textLink=""
-                                hrefLink=""
-                            >
-                                <input placeholder={"Professor"} type="Professor"/>
-                                <input placeholder={"Composer"} type="Composer"/>
-                                <input placeholder={"Subtitles languages"} type="Subtitles languages"/>
-                                <input placeholder={"Sort by"} type="Sort by"/>
-                                <div>
-                                    <a href="">Undo Filter</a>
-                                    <Button size={'sm'}>Filter</Button>
-                                </div>
-                            </Card>
-                        </div>
+                        <Card
+                            title="Filter"
+                            textLink=""
+                            hrefLink=""
+                        >
+                            <div className={style.mobile_filter_content}>
+                                <FormField
+                                    label="Professor"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="Composer"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="Composer"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="State"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                                <FormField
+                                    label="Display"
+                                    type="select"
+                                    size="md"
+                                    value=""
+                                />
+                            </div>
+                            <div className={style.mobile_submit_filter}>
+                                <a href="">Undo Filter</a>
+                                <Button size={'sm'}>Filter</Button>
+                            </div>
+                        </Card>
+
                     </div>
                     <Card title="Courses" textLink="" hrefLink="">
                         <div className={style.grid_courses}>
