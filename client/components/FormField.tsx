216ec5d157459489dc51d875cd2d12b1a2d73bde
Giselle MM <giselle.magalong@gmail.com>
2023-08-11 21:18:02 +0200

    🐞fix : map filter

--- client/components/FormField.tsx
+++ client/components/FormField.tsx
@@ -5,6 +5,7 @@ interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label: string;
     type: string;
     sizeInput: 'md' | 'lg';
+    value:string,
     onChange: (value) => void;
     icon?: React.ReactNode;
 };
