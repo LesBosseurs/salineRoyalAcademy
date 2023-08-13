f6d7f3783b1515c1afe2d49bdf462804a19c3c16
Florent Paris <florentparis28@gmail.com>
2023-08-13 16:21:52 +0200

    ✨ feat: BadgeInstrument

--- client/pages/profil.tsx
+++ client/pages/profil.tsx
@@ -1,10 +1,18 @@
 import Header from '@/components/common/Header';
+import style from '../styles/pages/profil.module.scss';
+import BadgeInstrument from '@/components/BadgeInstrument';
 
 export default function Profil() {
   return (
-    <div>
+    <div className={style.profil}>
       <Header name="Profil" />
-      <span>profil page</span>
+      <div className={style.general_infos}>
+        <div className={style.profil_pic}></div>
+        <div>
+          <h3>My name</h3>
+          <BadgeInstrument size="md" fill="#fff" instrument="piano" />
+        </div>
+      </div>
     </div>
   );
 }
