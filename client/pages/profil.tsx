5644a58eabb7ad0cfea4175e386c710475496a12
Florent Paris <florentparis28@gmail.com>
2023-08-13 11:49:55 +0200

    ✨ feat: NavMobile

---
+++ client/pages/profil.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/common/Header';
+
+export default function Profil() {
+  return (
+    <div>
+      <Header name="Profil" />
+      <span>profil page</span>
+    </div>
+  );
+}
