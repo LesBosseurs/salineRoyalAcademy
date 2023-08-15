c0580f333f967c79f8ec2e6630adbcece1215a3e
Giselle MM <giselle.magalong@gmail.com>
2023-08-15 20:04:53 +0200

    🦄 refactor: variables naming, suppress console.log

--- client/components/FormField.tsx
+++ client/components/FormField.tsx
@@ -2,58 +2,59 @@ import React, {ChangeEvent} from 'react';
 import style from '../styles/components/FormField.module.scss';
 
 interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    label: string;
-    type: string;
-    sizeInput: 'md' | 'lg';
-    value:string,
-    onChange: (value) => void;
-    icon?: React.ReactNode;
-};
+  label: string;
+  type: string;
+  sizeInput: 'md' | 'lg';
+  value:string,
+  onChange: (value) => void;
+  icon?: React.ReactNode;
+}
 
 const FormField = ({
-
-                       label,
-                       type,
-                       sizeInput,
-                       value,
-                       onChange,
-                       icon,
-                       ...props
-                   }:FormFieldProps ) => {
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const newValue = {
-            ...value,
-            [e.target.name]: e.target.value,
-        };
-        onChange(newValue);
+  label,
+  type,
+  sizeInput,
+  value,
+  onChange,
+  icon,
+  ...props
+  }:FormFieldProps ) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = {
+      ...value,
+      [e.target.name]: e.target.value,
     };
-    const aspectButton = () => {
-        switch (sizeInput) {
-            case 'md':
-                return style.md;
-            case 'lg':
-                return style.lg;
-        }
-    };
+    onChange(newValue);
+  };
 
-    return (
-        <div className={type !== "search"? style.FormField : style.searchField}>
-            {type !== "search" ? (
-                <label>
-                    {icon}
-                    {label.charAt(0).toUpperCase() + label.slice(1).replace(/_/g, ' ')}
-                </label>
-            ):(<>{icon}</>)}
-            <input
-                {...props}
-                type={type}
-                id={label}
-                name={label}
-                value={value[label]}
-                className={aspectButton()}
-                onChange={handleInputChange}
-            ></input>
-        </div>
-    );
+  const aspectButton = () => {
+    switch (sizeInput) {
+      case 'md':
+        return style.md;
+      case 'lg':
+        return style.lg;
+    }
+  };
+
+  return (
+    <div className={type !== "search"? style.FormField : style.searchField}>
+      {type !== "search" ? (
+        <label>
+          {icon}
+          {label.charAt(0).toUpperCase() + label.slice(1).replace(/_/g, ' ')}
+        </label>
+      ):(<>{icon}</>)}
+      <input
+        {...props}
+        type={type}
+        id={label}
+        name={label}
+        value={value[label]}
+        className={aspectButton()}
+        onChange={handleInputChange}
+      ></input>
+    </div>
+  );
 }
+
 export default FormField;
--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -1,29 +1,26 @@
 import React from "react";
 import style from "@/styles/components/Modal.module.scss";
-import Button from "@/components/Button";
-import Card from "@/components/Card";
 import Cross from "@/public/icons/cross";
 
 type ModalProps = {
-    setOpenModal: (b: boolean) => void;
-    open: boolean;
-    children: React.ReactNode;
+  setOpenModal: (state: boolean) => void;
+  open: boolean;
+  children: React.ReactNode;
 };
 
-export default function Modal({setOpenModal, open, children}: ModalProps){
-    //console.log(open);
-    if(!open){
-        return "";
-    }
-    return(
-        <div className={style.modal}>
-            <div  className={style.modal_card}>
-                <div className={style.close_icon} onClick={() => setOpenModal(false)}>
-                    <Cross></Cross>
-                </div>
-                <div className={style.separator}></div>
-                <h2>Filter</h2>
-                {children}
-            </div>
-        </div>)
+export default function Modal({setOpenModal, open, children}: ModalProps) {
+  if (!open) {
+    return "";
+  }
+  return (
+    <div className={style.modal}>
+      <div className={style.modal_card}>
+        <div className={style.close_icon} onClick={() => setOpenModal(false)}>
+          <Cross fill={'#000'}/>
+        </div>
+        <div className={style.separator}></div>
+        <h2>Filter</h2>
+        {children}
+      </div>
+    </div>)
 }
