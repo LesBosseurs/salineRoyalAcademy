58eeea945a15a3f1034ce45e2cbfd5f02b3a956d
Florent Paris <florentparis28@gmail.com>
2023-08-21 00:13:13 +0200

    🌈 style: wip course-page

--- client/pages/courses/[courseId].tsx
+++ client/pages/courses/[courseId].tsx
@@ -11,6 +11,8 @@ import BadgeInstrument from '@/components/BadgeInstrument';
 import Link from 'next/link';
 import CapRoundedIcon from '@/public/icons/others/CapRounded';
 import DownloadIcon from '@/public/icons/others/Download';
+import Image from 'next/image';
+import QuoteIcon from '@/public/icons/others/Quote';
 
 interface MasterclassProps {
   title: string;
@@ -45,7 +47,10 @@ export default function Course() {
           <h2>{masterclass.title}</h2>
           <Button size="xs">See all chapters</Button>
         </div>
-        <Card title="Chapitre 1 | Beethoven : Sonate pour piano n°1">
+        <Card
+          title="Chapitre 1 | Beethoven : Sonate pour piano n°1"
+          style={{ gridColumn: '1/5' }}
+        >
           <div className={style.player}>
             <div>
               <span>published 2h ago</span>
@@ -60,17 +65,17 @@ export default function Course() {
             </div>
           </div>
         </Card>
-        <Card title="Description">
+        <Card style={{ gridColumn: '1/5', gridRow: '3/5' }} title="Description">
           <div className={style.description}>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Interdum consectetur libero id faucibus...
             </p>
-            <Link href="/">See more...</Link>
+            <span>See more...</span>
           </div>
         </Card>
-        <Card title="What we learn">
+        <Card style={{ gridColumn: '1/3' }} title="What we learn">
           <ul className={style.future_knowledge}>
             <li>
               <CapRoundedIcon fill="#000" />
@@ -86,17 +91,32 @@ export default function Course() {
             </li>
           </ul>
         </Card>
-        <Card title="Content/ressources">
-          <DownloadIcon fill="#000" />
+        <Card style={{ gridColumn: '3/5' }} title="Content/ressources">
+          <div className={style.ressources}>
+            <DownloadIcon fill="#000" />
+            <Image
+              width={100}
+              height={40}
+              alt="ressources image"
+              src="/prov/partoch.png"
+            />
+          </div>
+        </Card>
+        <Card style={{ gridColumn: '5/7', gridRow: '3/4' }}>
+          <div className={style.quote}>
+            <QuoteIcon fill="#989595" />
+            <p>
+              When you know what you play, colors come naturally and you don’t
+              have to make it happen.
+            </p>
+            <span>Adelie Quantin</span>
+            <QuoteIcon fill="#989595" />
+          </div>
+        </Card>
+        <Card style={{ gridColumn: '5/7', gridRow: '4/5' }}>
           <span>Test</span>
         </Card>
-        <Card>
-          <span>Test</span>
-        </Card>
-        <Card>
-          <span>Test</span>
-        </Card>
-        <Card>
+        <Card style={{ gridColumn: '5/7' }}>
           <span>Test</span>
         </Card>
       </CardContainer>
