ec1ad96074953e36a6803d82cfcc51d0cde8f911
dbnsf <d_bruneau2@hetic.eu>
2023-09-06 10:49:45 +0200

    🐞 fix: fixing display issues

--- client/pages/person.tsx
+++ client/pages/person.tsx
@@ -8,6 +8,7 @@ import CardContainer from '@/components/CardContainer'
 import Card from '@/components/Card'
 import ActivityCard from '@/components/ActivityCard'
 import CourseCard from '@/components/CourseCard'
+import ArticleCard from '@/components/ArticleCard'
 import CompetitionCard from '@/components/CompetitionCard'
 import CapIcon from '@/public/icons/cap'
 import VideoCard from '@/components/VideoCard'
@@ -16,35 +17,54 @@ import { Instruments } from '../types/instruments'
 export default function Person () {
   return (
     <div className={style.person}>
-      <Header name='Person' />
-      <div className={style.general_infos}>
-        <figure
-          className={style.person_pic}
-          style={{
-            backgroundImage: `url(https://tse4.mm.bing.net/th?id=OIP.PiKTYKOuZn_6SQvrMOLpigHaFI&pid=Api)`
-          }}
-        />
-        <div className={style.name_and_instrument}>
-          <div className={style.instrument_list}>
-            <BadgeInstrument size='lg' fill='#fff' instrument={Instruments.Violin} />
-            <BadgeInstrument size='lg' fill='#fff' instrument={Instruments.Piano} />
-            <BadgeInstrument size='lg' fill='#fff' instrument={Instruments.Cello} />
-          </div>
-          <h3>Svetlana Makarova</h3>
-        </div>
-        <div className={style.tag_list}>
-          <TagUser size='sm'>Teacher</TagUser>
-          <TagUser size='sm'>Student</TagUser>
-        </div>
-        <div className={style.main_infos}>
-          <span>Nationality: Russian</span>
-          <span>Instruments: Violin, Piano, Oboe</span>
-        </div>
-      </div>
+      <Header name='Svetlana Makarova' />
       <CardContainer>
-        <Card style={{ gridColumn: '1/5' }} title='Participated academies'>
-          <div className={style.participated_academies_container}>
-            <div className={style.academies_card}>
+        {/* <Card> */}
+        <div className={style.general_infos}>
+          {/* <Card> */}
+          <figure
+            className={style.person_pic}
+            style={{
+              backgroundImage: `url(https://tse4.mm.bing.net/th?id=OIP.PiKTYKOuZn_6SQvrMOLpigHaFI&pid=Api)`
+            }}
+          />
+          {/* </Card> */}
+          <div className={style.name_and_instrument}>
+            <div className={style.instrument_list}>
+              <BadgeInstrument
+                size='lg'
+                fill='#fff'
+                instrument={Instruments.Violin}
+              />
+              <BadgeInstrument
+                size='lg'
+                fill='#fff'
+                instrument={Instruments.Piano}
+              />
+              <BadgeInstrument
+                size='lg'
+                fill='#fff'
+                instrument={Instruments.Cello}
+              />
+            </div>
+            <h3>Svetlana Makarova</h3>
+          </div>
+          <Card textLink='' hrefLink=''>
+            <div className={style.infos_details}>
+              <div className={style.tag_list}>
+                <TagUser size='sm'>Teacher</TagUser>
+                <TagUser size='sm'>Student</TagUser>
+              </div>
+              {/* <div className={style.main_infos}> */}
+              <span>Nationality: Russian</span>
+              <span>Instruments: Violin, Piano, Oboe</span>
+            </div>
+            {/* </div> */}
+          </Card>
+        </div>
+        {/* </Card> */}
+          <Card style={{ gridColumn: '1' }} title='Participated academies'>
+            {/* <div className={style.academies_card}> */}
               <CompetitionCard
                 instrument={Instruments.Piano}
                 title='The Queen Sonja International Music Competition'
@@ -52,14 +72,40 @@ export default function Person () {
                 date={new Date('2023-08-31T13:12:00.838Z')}
                 award={50000}
               />
-            </div>
-          </div>
-        </Card>
+            {/* </div> */}
+          </Card>
         <Card
-          style={{ gridColumn: '5/7', gridRow: '1/4' }}
+          style={{ gridColumn: '1', gridRow: '1/4' }}
           title='Related masterclasses'
+          textLink='see all masterclasses'
+          hrefLink=''
         >
-          <div className={style.related_masterclasses_container}></div>
+          <div className={style.related_masterclasses_container}>
+            <CourseCard
+              title='Fantasy in C'
+              professor='Robert Shumann'
+              instrument={Instruments.Piano}
+              difficulty={2}
+              actualChapter='Chap. 2'
+              pourcentage={20}
+            />
+            <CourseCard
+              title='Fantasy in C'
+              professor='Robert Shumann'
+              instrument={Instruments.Piano}
+              difficulty={2}
+              actualChapter='Chap. 2'
+              pourcentage={20}
+            />
+            <CourseCard
+              title='Fantasy in C'
+              professor='Robert Shumann'
+              instrument={Instruments.Piano}
+              difficulty={2}
+              actualChapter='Chap. 2'
+              pourcentage={20}
+            />
+          </div>
         </Card>
         <Card
           style={{ gridColumn: '5/7', gridRow: '3/4' }}
@@ -78,11 +124,46 @@ export default function Person () {
           </div>
         </Card>
         <Card
-          style={{ gridColumn: '5/7', gridRow: '4/4' }}
+          style={{ gridColumn: '1', gridRow: '4/4' }}
           title='Related articles'
         >
           <div className={style.related_articles_container}>
-            <div className={style.article_card}></div>
+            <ArticleCard
+              instrument={Instruments.Piano}
+              title='Where to listen classical music at Paris'
+              author='parisoperafan'
+              date={new Date('Thu Jul 06 2023 19:41:21 GMT+0200')}
+            />
+            <ArticleCard
+              instrument={Instruments.Piano}
+              title='Where to listen classical music at Paris'
+              author='parisoperafan'
+              date={new Date('Thu Jul 06 2023 19:41:21 GMT+0200')}
+            />
+            <ArticleCard
+              instrument={Instruments.Piano}
+              title='Where to listen classical music at Paris'
+              author='parisoperafan'
+              date={new Date('Thu Jul 06 2023 19:41:21 GMT+0200')}
+            />
+            <ArticleCard
+              instrument={Instruments.Piano}
+              title='Where to listen classical music at Paris'
+              author='parisoperafan'
+              date={new Date('Thu Jul 06 2023 19:41:21 GMT+0200')}
+            />
+            <ArticleCard
+              instrument={Instruments.Piano}
+              title='Where to listen classical music at Paris'
+              author='parisoperafan'
+              date={new Date('Thu Jul 06 2023 19:41:21 GMT+0200')}
+            />
+            <ArticleCard
+              instrument={Instruments.Piano}
+              title='Where to listen classical music at Paris'
+              author='parisoperafan'
+              date={new Date('Thu Jul 06 2023 19:41:21 GMT+0200')}
+            />
           </div>
         </Card>
       </CardContainer>
