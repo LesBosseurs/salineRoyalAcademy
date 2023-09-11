089962589e68b5db96be48e05a75566e9a429938
Giselle MM <giselle.magalong@gmail.com>
2023-09-11 10:11:56 +0200

    🦄Refactor: add badge notification card

---
+++ client/public/icons/others/medal.tsx
@@ -0,0 +1,8 @@
+export default function Medal({ fill }: any) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="26" viewBox="0 0 19 26" fill="none">
+      <path d="M9.5 16.7408C13.9183 16.7408 17.5 13.2918 17.5 9.03708C17.5 4.78244 13.9183 1.33337 9.5 1.33337C5.08172 1.33337 1.5 4.78244 1.5 9.03708C1.5 13.2918 5.08172 16.7408 9.5 16.7408Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4.19054 14.9866L4.17871 23.7333C4.17871 24.8 4.92538 25.3215 5.84982 24.8829L9.02611 23.3778C9.28685 23.2474 9.72537 23.2474 9.98611 23.3778L13.1743 24.8829C14.0868 25.3096 14.8454 24.8 14.8454 23.7333V14.7733" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  );
+}
